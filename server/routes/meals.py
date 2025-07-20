a9e0f73e0a0edd9c4c80acf783f65dd02f77acce
Peter Bwire <peterbwirea@gmail.com>
2025-07-20 16:24:55 +0300

    get all meals

--- server/routes/meals.py
+++ server/routes/meals.py
@@ -3,3 +3,8 @@ from models import db, Meal, Menu
 from sqlalchemy.exc import IntegrityError
 
 meals_bp = Blueprint('meals', __name__, url_prefix='/meals')
+
+@meals_bp.route('/', methods=['GET'])
+def get_meals():
+    meals = Meal.query.all()
+    return jsonify([meal.to_dict() for meal in meals]), 200
