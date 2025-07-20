0c71039bbe999b0e26046af2abbf0b6533cd3cd0
Peter Bwire <peterbwirea@gmail.com>
2025-07-20 16:25:41 +0300

    get a specific meal

--- server/routes/meals.py
+++ server/routes/meals.py
@@ -8,3 +8,10 @@ meals_bp = Blueprint('meals', __name__, url_prefix='/meals')
 def get_meals():
     meals = Meal.query.all()
     return jsonify([meal.to_dict() for meal in meals]), 200
+
+@meals_bp.route('/<int:id>', methods=['GET'])
+def get_meal(id):
+    meal = Meal.query.get(id)
+    if not meal:
+        return jsonify({"error": "Meal not found"}), 404
+    return jsonify(meal.to_dict()), 200
