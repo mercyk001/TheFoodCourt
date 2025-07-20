57e1312402451a063ea88b6f8c7075a7b2963095
Peter Bwire <peterbwirea@gmail.com>
2025-07-20 16:32:48 +0300

    get all tables

--- server/routes/tables.py
+++ server/routes/tables.py
@@ -4,3 +4,8 @@ from models import db, Table, Reservation
 from datetime import datetime
 
 tables_bp = Blueprint('tables', __name__, url_prefix='/tables')
+
+@tables_bp.route('/', methods=['GET'])
+def get_tables():
+    tables = Table.query.all()
+    return jsonify([table.to_dict() for table in tables]), 200
