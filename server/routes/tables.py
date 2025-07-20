b2180e4ed662b4840d5c764023ef6b9936e4d1de
Peter Bwire <peterbwirea@gmail.com>
2025-07-20 16:31:03 +0300

    table blueprint

--- server/routes/tables.py
+++ server/routes/tables.py
@@ -2,3 +2,5 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required
 from models import db, Table, Reservation
 from datetime import datetime
+
+tables_bp = Blueprint('tables', __name__, url_prefix='/tables')
