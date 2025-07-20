7b1d820f83f3bffddabaa9f141bb13ef86687e01
Peter Bwire <peterbwirea@gmail.com>
2025-07-20 16:33:34 +0300

    get a specific table

--- server/routes/tables.py
+++ server/routes/tables.py
@@ -9,3 +9,10 @@ tables_bp = Blueprint('tables', __name__, url_prefix='/tables')
 def get_tables():
     tables = Table.query.all()
     return jsonify([table.to_dict() for table in tables]), 200
+
+@tables_bp.route('/<int:id>', methods=['GET'])
+def get_table(id):
+    table = Table.query.get(id)
+    if not table:
+        return jsonify({"error": "Table not found"}), 404
+    return jsonify(table.to_dict()), 200
