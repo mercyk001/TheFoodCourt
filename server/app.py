95507d4dc82b9087f0c26c1c2998211efb5678f3
MERCY CHEPKEMOI <koechmercyk@gmail.com>
2025-07-29 23:23:40 +0300

    Update origins

--- server/app.py
+++ server/app.py
@@ -37,7 +37,7 @@ migrate = Migrate(app, db)
 
 # Configure CORS with more explicit settings
 CORS(app, 
-         origins=['http://localhost:3000', 'http://localhost:5555'],
+         origins=['http://localhost:3000', 'https://thefoodcourt-5.onrender.com'],
          supports_credentials=True,
          allow_headers=['Content-Type', 'Authorization', 'Cookie'],
          expose_headers=['Set-Cookie'],
